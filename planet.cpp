d4b03bc511033d71975ba70043c0a90607e2445e
Samuel <samuellavoie90@hotmail.com>
2011-05-02 09:21:30 -0400

    Commit 2

--- planet.cpp
+++ planet.cpp
@@ -3,4 +3,48 @@
 Planet::Planet(QObject *parent) :
     QObject(parent)
 {
+    //QPoint Location;
+    //int Population;
+    //int Player;
+   // int PopulationGrowth;
+   // bool Focus;
+}
+void Planet::initialize(int Owner, int Quadrant, Planet *PlanetArray, int PlanetCount, int PlanetSize)
+{
+    int i =0;
+Player=Owner;
+bool Collision = false;
+PopulationGrowth = PlanetSize;
+int sizex = 512;
+int sizey = 384;
+if(Quadrant!=1)
+{
+    int sizex = 512;
+    int sizey = 384;
+}
+
+do
+{
+    Collision = false;
+Location=QPoint(qrand()%sizex,qrand()%sizey);
+while(i<PlanetCount)
+{
+    if(CheckPlanetToPlanetCollision(PlanetArray[i]))
+    {
+        Collision = true;
+    }
+           // qrand();
+    i++;
+}
+
+}while(Collision);
+
+}
+void Planet::MirrorPlanet(Planet SomePlanet,int Quadrant)
+{
+
+}
+bool CheckPlanetToPlanetCollision(Planet planet2)
+{
+
 }
--- planet.h
+++ planet.h
@@ -12,15 +12,17 @@ public:
     int Population;
     int Player;
     int PopulationGrowth;
+    bool Focus;
     //populationGrowth va etre utiliser pour determiner la grandeur de la planete.
     //une variable de texture
    //int planettype;
-    bool Focus;
+
 signals:
 
 public slots:
-    void initialize(int Player, int Quadrant, Planet *PlanetArray,int PlanetCount);
+    void initialize(int Owner, int Quadrant, Planet *PlanetArray,int PlanetCount);
     void MirrorPlanet(Planet SomePlanet,int Quadrant);
+    bool CheckPlanetToPlanetCollision(Planet planet1, Planet planet2);
 
 };
 

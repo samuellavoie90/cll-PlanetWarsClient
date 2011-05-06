25914edd15726f55e3cc14f6044bc88fc7797579
Samuel <samuellavoie90@hotmail.com>
2011-05-06 15:52:28 -0400

    MagiRect Tout ecrit sauf le draw

--- magicrect.cpp
+++ magicrect.cpp
@@ -3,3 +3,31 @@
 MagicRect::MagicRect()
 {
 }
+QRect MagicRect::GetRectangle()
+{
+    QRect temp;
+    if(TopLeft.y()>BotRight.y())
+    {
+        if(TopLeft.x()>BotRight.x())
+        {
+            temp=QRect(BotRight,TopLeft);
+        }
+        else
+        {
+            temp=QRect(TopLeft.x(),BotRight.y(),TopLeft.x()-BotRight.x(),BotRight.y()-TopLeft.y());
+        }
+    }
+    else
+    {
+        if(TopLeft.x()>BotRight.x())
+        {
+            temp=QRect(BotRight.x(),TopLeft.y(),BotRight.x()-TopLeft.x(),TopLeft.y()-BotRight.y());
+        }
+        else
+        {
+            temp=QRect(TopLeft,BotRight);
+        }
+
+    }
+    return temp;
+}
--- magicrect.h
+++ magicrect.h
@@ -6,6 +6,11 @@ class MagicRect
 {
 public:
     MagicRect();
+    QPoint TopLeft;
+    QPoint BotRight;
+    QRect Rectangle1;
+    QRect GetRectangle();
+    void Draw();
 
 };
 
